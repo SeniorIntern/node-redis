21f9b88ae1cf461d9b67410928d2215925ca548b
Nikhil Thapa <tnikhil.official@gmail.com>
2023-07-08 23:58:22 +0545

    [Invalidating Cache] : Setting expire of 60 on keys for maintaining correct data.

--- index.js
+++ index.js
@@ -7,26 +7,33 @@ app.use(express.json());
 app.post('/data', async (req, res) => {
   const repo = req.body.repo;
 
+  let timeStart = Date.now();
   const value = await redis.get(repo);
   if (value) {
+    const timeEnd = Date.now();
     return res.json({
       from: 'redis',
       status: 'ok',
       stars: value,
+      timeForRet: timeEnd - timeStart,
     });
   }
 
+  timeStart = Date.now(); // re-initialize again for remote
   const response = await fetch(`https://api.github.com/repos/${repo}`).then(
     (t) => t.json()
   );
 
+  const timeEnd = Date.now();
+
   if (response.stargazers_count != undefined)
-    await redis.set(repo, response.stargazers_count);
+    await redis.setex(repo, 60, response.stargazers_count);
 
   res.json({
     from: 'remote',
     status: 'ok',
     stars: response.stargazers_count,
+    timeForRet: timeEnd - timeStart,
   });
 });
 
